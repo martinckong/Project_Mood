6d0b0f22083f9179d10987b449e547ffe351215c
Martin Kong <martinckong24@gmail.com>
2017-01-21 11:30:50 -0800

    loops to create output jsons

--- tone2.py
+++ tone2.py
@@ -10,34 +10,30 @@ tone_analyzer = ToneAnalyzerV3(
 
 import sys, getopt
 
-def main(argv):
+def runscript(s1, s2):
    inputfile = ''
    outputfile = ''
-   try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
-   except getopt.GetoptError:
-      print('test.py -i <inputfile> -o <outputfile>')
-      sys.exit(2)
-   for opt, arg in opts:
-      if opt == '-h':
-         print('test.py -i <inputfile> -o <outputfile>')
-         sys.exit()
-      elif opt in ("-i", "--ifile"):
-         inputfile = arg
-      elif opt in ("-o", "--ofile"):
-         outputfile = arg
    
+   if s1 != '':
+      inputfile = s1
+   if s2 != '':
+      outputfile = s2
+
    input = open(inputfile, "r")
-   parsetext = input.read();
+   firstline = input.readline().rstrip()
+   firstline = firstline.split(",")
+   songtitle = '\t"songtitle": "' + firstline[0][:-1] + '",\n'
+   artist = '\t"artist": "' + firstline[1][1:] + '",\n'
+   parsetext = input.read()
    
    if outputfile != '':
       output = open(outputfile, "w")
-      output.write(json.dumps(tone_analyzer.tone(text=parsetext), indent=2))
+      output.write("{\n")
+      output.write(songtitle)
+      output.write(artist)
+      output.write(json.dumps(tone_analyzer.tone(text=parsetext)['document_tone'], indent=2)[2:])
       output.close()
    else:
-      print(json.dumps(tone_analyzer.tone(text=parsetext), indent=2))
+      print(json.dumps(tone_analyzer.tone(text=parsetext)['document_tone'], indent=2))
 
    input.close()
-
-if __name__ == "__main__":
-   main(sys.argv[1:])
